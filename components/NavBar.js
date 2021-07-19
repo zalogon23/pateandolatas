7097e9d85fb94633dd2929f35693355ef5e8ab4e
ElTioGonza <gonz230300@gmail.com>
2021-07-19 13:38:07 -0300

    Deploy

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,19 +6,19 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <HStack py="1" px="10" w="full" bg="red.800" spacing="10" justifyContent={["space-around", "flex-end"]}>
+    <HStack py="1" px="6" w="full" bg="red.800" spacing={["3", "10"]} justifyContent={["space-around", "flex-end"]}>
       <Link href="/">
-        <Button leftIcon={<FontAwesomeIcon icon={faHome} />} aria-label="Ir a inicio" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+        <Button leftIcon={<FontAwesomeIcon icon={faHome} />} aria-label="Ir a inicio" fontSize={["md", "lg", "xl"]} bgColor="red.800" color="orange.200" variant="solid">
           Inicio
         </Button>
       </Link>
       <Link href="/guias">
-        <Button leftIcon={<FontAwesomeIcon icon={faRoute} />} aria-label="Ir a guias" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+        <Button leftIcon={<FontAwesomeIcon icon={faRoute} />} aria-label="Ir a guias" fontSize={["md", "lg", "xl"]} bgColor="red.800" color="orange.200" variant="solid">
           Guías
         </Button>
       </Link>
       <Link href="/locales">
-        <Button leftIcon={<FontAwesomeIcon icon={faMosque} />} aria-label="Ir a locales" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+        <Button leftIcon={<FontAwesomeIcon icon={faMosque} />} aria-label="Ir a locales" fontSize={["md", "lg", "xl"]} bgColor="red.800" color="orange.200" variant="solid">
           Locales
         </Button>
       </Link>
