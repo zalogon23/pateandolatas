5ca4644d53d341e1bdb252a8c9c61b1214d3dbec
ElTioGonza <gonz230300@gmail.com>
2021-07-19 13:28:04 -0300

    Map sizing fix. And Added words to Nav Bar COMPONENT items

--- components/Header.js
+++ components/Header.js
@@ -31,7 +31,7 @@ function Header() {
         <Link href="/">
           <IconButton aria-label="Ir a inicio" rounded="full" bgColor="red.700" variant="ghost" w={["2.4rem",, "3.1rem"]} h={["2.4rem",, "3.1rem"]} position="absolute" left={["1.5rem", , "3rem"]} top="27%">
             <Circle transitionDuration="300ms" _hover={{ bgColor: "yellow.300" }} cursor="pointer" w={["2.3rem",, "3rem"]} h={["2.3rem",, "3rem"]} bgColor="orange.500">
-              <Image transitionDuration="350ms" transform="auto" _hover={{ rotate: "-55deg" }} w={["3rem",, "4.5rem"]} h={["3rem",, "4.5rem"]} src="/logo.png" maxWidth="9999px" />
+              <Image transitionDuration="350ms" transform="auto" _hover={{ rotate: "-55deg" }} w={["3rem",, "4.5rem"]} h={["2.8rem",, "4.1rem"]} src="/logo.png" maxWidth="9999px" />
             </Circle>
           </IconButton>
         </Link>
@@ -39,7 +39,7 @@ function Header() {
       </HStack>
       <NavBar />
       {scrolled &&
-        <IconButton aria-label="Ir a la parte superior de la pagina" zIndex="popover" onClick={scrollUp} boxShadow="0 0 0.4rem #000" rounded="full" position="fixed" w="14" h="14" right="4" bottom="6" bgColor="orange.200">
+        <IconButton aria-label="Ir a la parte superior de la pagina" zIndex="popover" onClick={scrollUp} boxShadow="0 0 0.4rem #000" rounded="full" position="fixed" w="14" h="14" right="4" bottom="20" bgColor="orange.200">
           <FontAwesomeIcon style={{ fontSize: "3rem" }} icon={faCaretUp} />
         </IconButton>
       }
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,4 @@
-import { IconButton, HStack } from '@chakra-ui/react'
+import { Button, HStack } from '@chakra-ui/react'
 import Link from "next/link"
 import { faHome, faMosque, faRoute } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,19 +8,19 @@ function NavBar() {
   return (
     <HStack py="1" px="10" w="full" bg="red.800" spacing="10" justifyContent={["space-around", "flex-end"]}>
       <Link href="/">
-        <IconButton aria-label="Ir a inicio" marginInline="0" fontSize={["lg", "xl", "2xl"]} bgColor="red.800" color="orange.200" variant="solid">
-          <FontAwesomeIcon icon={faHome} />
-        </IconButton>
+        <Button leftIcon={<FontAwesomeIcon icon={faHome} />} aria-label="Ir a inicio" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+          Inicio
+        </Button>
       </Link>
       <Link href="/guias">
-        <IconButton aria-label="Ir a guias" marginInline="0" fontSize={["lg", "xl", "2xl"]} bgColor="red.800" color="orange.200" variant="solid">
-          <FontAwesomeIcon icon={faRoute} />
-        </IconButton>
+        <Button leftIcon={<FontAwesomeIcon icon={faRoute} />} aria-label="Ir a guias" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+          Guías
+        </Button>
       </Link>
       <Link href="/locales">
-        <IconButton aria-label="Ir a locales" marginInline="0" fontSize={["lg", "xl", "2xl"]} bgColor="red.800" color="orange.200" variant="solid">
-          <FontAwesomeIcon icon={faMosque} />
-        </IconButton>
+        <Button leftIcon={<FontAwesomeIcon icon={faMosque} />} aria-label="Ir a locales" marginInline="0" fontSize={["md", "lg","xl"]} bgColor="red.800" color="orange.200" variant="solid">
+          Locales
+        </Button>
       </Link>
     </HStack>
   )
