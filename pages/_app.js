dfede72ad23ba36534aa01405a2f17f6bcd1b6ea
ElTioGonza <gonz230300@gmail.com>
2021-07-20 12:39:37 -0300

    Deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import "@fontsource/raleway/700.css"
-import "@fontsource/raleway/400.css"
 import theme from "../theme"
 
 function MyApp({ Component, pageProps }) {
