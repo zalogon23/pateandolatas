409b804b4e17016d6d64271994d0fc5ac73a579e
ElTioGonza <gonz230300@gmail.com>
2021-07-21 11:58:00 -0300

    Deploy

--- components/Share.js
+++ components/Share.js
@@ -8,15 +8,15 @@ function Share({ url }) {
   return (
     <Box pb="4">
       <Heading as="h4" fontSize="xl" py="12" px="2">Si te gusto el contenido puedes compartirlo con tus amigos y familiares:</Heading>
-      <ButtonGroup isAttached bgColor="black" justifyContent="center" w="full" color="white">
+      <Box color="white" w="full" d="flex" justifyContent="center" bg="black">
         <FacebookShareButton url={url}><Button fontSize={["sm", , "md", "xl"]} p="2" aria-label="Compartir por Facebook " rounded="0" bg="blue.500" _hover={{ filter: "brightness(120%)" }} leftIcon={<FontAwesomeIcon icon={faFacebook} />}>Compartir</Button></FacebookShareButton>
         <FacebookMessengerShareButton url={url}><Button fontSize={["sm", , "md", "xl"]} p="2" aria-label="Compartir por Facebook Messenger " rounded="0" bg="blue.300" _hover={{ filter: "brightness(120%)" }} leftIcon={<FontAwesomeIcon icon={faFacebookMessenger} />}>Mostrar</Button></FacebookMessengerShareButton>
         <WhatsappShareButton url={url}><Button fontSize={["sm", , "md", "xl"]} p="2" aria-label="Compartir por Whatsapp " rounded="0" bg="green.500" _hover={{ filter: "brightness(120%)" }} leftIcon={<FontAwesomeIcon icon={faWhatsapp} />}>Enviar</Button></WhatsappShareButton>
-      </ButtonGroup>
+      </Box>
     </Box>
   )
 }
 
-//algo
+//deploy
 
 export default Share
